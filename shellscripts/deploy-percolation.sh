a34647f6c9d41279f3ba52eef697e86a2939857c
Yudi Yamane <yudiyamane@gmail.com>
2020-07-06 18:33:40 -0300

    script remove a linha com padrão package

--- shellscripts/deploy-percolation.sh
+++ shellscripts/deploy-percolation.sh
@@ -5,5 +5,7 @@ cd ./src/main/java/AlgorithmsPrincetonCourse/percolation
 mkdir -p percolation
 cp Percolation.java ./percolation
 cp PercolationStats.java ./percolation
+sed -i '/package AlgorithmsPrincetonCourse.percolation;/d' ./percolation/Percolation.java
+sed -i '/package AlgorithmsPrincetonCourse.percolation;/d' ./percolation/PercolationStats.java
 zip -r percolation.zip ./percolation
 rm -r ./percolation
