73f5a04d1679ea567049d9b5279d310cb5a9c69a
Yudi Yamane <yudiyamane@gmail.com>
2020-07-17 17:24:47 -0300

    ainda falta testar

---
+++ src/main/java/AlgorithmsPrincetonCourse/Assignments/queues/RandomizedQueue.java
@@ -0,0 +1,98 @@
+package AlgorithmsPrincetonCourse.Assignments.queues;
+
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+@SuppressWarnings("unchecked")
+public class RandomizedQueue<Item> {
+  private int capacity, head, tail;
+  private Item[] queue;
+
+  // construct an empty randomized queue
+  public RandomizedQueue() {
+    head = 0;
+    tail = 0;
+    capacity = 1;
+    queue = (Item[]) new Object[capacity];
+  }
+
+  // is the randomized queue empty?
+  public boolean isEmpty() {
+    return head == tail;
+  }
+
+  // return the number of items on the randomized queue
+  public int size() {
+    return tail - head;
+  }
+
+    // add the item
+  public void enqueue(Item item) {
+    if (tail == capacity-1) {
+      resize(capacity*2);
+    }
+
+    queue[tail++] = item;
+  }
+
+  // remove and return a random item
+  public Item dequeue() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+
+    int randomPosition = StdRandom.uniform(head, tail);
+    Item item = queue[randomPosition];
+    reformatAt(randomPosition);
+
+    int size = size();
+    if (size > 0 && size == capacity/4) {
+      resize(capacity/2); // resizeSmaller
+    }
+
+    if (isEmpty()) {
+      resize(1); // resizeSmaller
+    }
+
+    return item;
+  }
+
+  private void reformatAt(int randomPosition) {
+    for (int i = randomPosition+1; i < tail; i++) {
+      queue[i-1] = queue[i];
+    }
+
+    tail--;
+  }
+
+  // return a random item (but do not remove it)
+  public Item sample() {
+    int randomPosition = StdRandom.uniform(head, tail);
+    return queue[randomPosition];
+  }
+
+  // resize array queue
+  private void resize(int newCapacity) {
+    int copyIndex;
+    Item[] copy = (Item[]) new Object[newCapacity];
+    
+    if (newCapacity < capacity) { // resizing to 1/4
+      for (int originalIndex = head; originalIndex < tail; originalIndex++) {
+        copyIndex = originalIndex - head; // -head is equivalent to the displacement/shift 
+                                          // to the left of the whole array
+        copy[copyIndex] = queue[originalIndex];
+      }
+      tail -= head;
+      head = 0;
+    }
+    else {
+      for (int i = head; i < tail; i++) {
+        copy[i] = queue[i];
+      }
+    }
+    
+    capacity = newCapacity;
+    queue = copy;
+  }
+}
