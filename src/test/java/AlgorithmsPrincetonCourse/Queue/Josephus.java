b562f5ec3545d71911a5d2de54f5b1b0d2caeac1
Yudi Yamane <yudiyamane@gmail.com>
2020-07-22 13:52:44 -0300

    attempt #1

--- src/test/java/AlgorithmsPrincetonCourse/Queue/Josephus.java
+++ src/test/java/AlgorithmsPrincetonCourse/Queue/Josephus.java
@@ -1,5 +1,62 @@
 package AlgorithmsPrincetonCourse.Queue;
 
+import edu.princeton.cs.algs4.Queue;
+
 public class Josephus {
-  
+  public static Queue<Person> josephus(int n, int m) {
+    Queue<Person> circle = new Queue<>();
+    Queue<Person> eliminatedPeople = new Queue<>();
+
+    for (int i = 0; i < n; i++) {
+      circle.enqueue(new Person(i));
+    }
+
+    int mCounter = 1;
+    while (eliminatedPeople.size() != n) {
+      for (Person person : circle) {
+        if (!person.isEliminated() && (mCounter == m)) {
+          if (mCounter == m) {
+            person.eliminate();
+            eliminatedPeople.enqueue(person);
+            mCounter = 1;
+          }
+        }
+        else {
+          mCounter++;
+        }
+      }
+    }
+
+    return eliminatedPeople;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(josephus(9, 3));
+  }
+
+  public static class Person {
+    private int id;
+    private boolean isEliminated;
+
+    Person(int id) {
+      this.id = id;
+      isEliminated = false;
+    }
+
+    public int id() {
+      return id;
+    }
+
+    public boolean isEliminated() {
+      return isEliminated;
+    }
+
+    public void eliminate() {
+      isEliminated = true;
+    }
+
+    public String toString() {
+      return "" + id;
+    }
+  }
 }
