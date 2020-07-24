3d9fd077bb2a284ee02af1305ab7613cb74e332f
Yudi Yamane <yudiyamane@gmail.com>
2020-07-24 10:57:01 -0300

    modified queue data type parameters in tests

--- src/test/java/AlgorithmsPrincetonCourse/Queue/CopyQueueTest.java
+++ src/test/java/AlgorithmsPrincetonCourse/Queue/CopyQueueTest.java
@@ -25,27 +25,20 @@ public class CopyQueueTest {
   
   @Test
   public void testIndepence() {
-    LinkedListQueue<String> source = new LinkedListQueue<>();
-    fillWithStrings(source, 10);
+    LinkedListQueue<Integer> source = new LinkedListQueue<>();
+    fillWithInts(source, 10);
     
-    LinkedListQueue<String> copy = new LinkedListQueue<>(source);
+    LinkedListQueue<Integer> copy = new LinkedListQueue<>(source);
     assertTrue(source != copy);
 
     assertTrue(copy.equals(source));
 
     assertTrue(copy.isCopyOf(source));
-    
   }
 
   public void fillWithInts(LinkedListQueue<Integer> queue, int n) {
     for (int i = 0; i < n; i++) {
       queue.enqueue(i);
-    }
-  }
-
-  public void fillWithStrings(LinkedListQueue<String> queue, int n) {
-    for (int i = 0; i < n; i++) {
-      queue.enqueue(Integer.toString(i));
     }
   }
 }
