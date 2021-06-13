8c9370355f489093830a238bad8668cd58c11760
Yudi Yamane <yudiyamane@gmail.com>
2021-06-13 20:34:48 -0300

    more exercises

---
+++ src/main/java/AlgorithmsPrincetonCourse/AbstractData/FlipsMax.java
@@ -0,0 +1,29 @@
+package AlgorithmsPrincetonCourse.AbstractData;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class FlipsMax {
+  public static Counter max(Counter x, Counter y) {
+    if (x.tally() > y.tally())
+      return x;
+    return y;
+  }
+
+  public static void main(String[] args) {
+    int T = 1000;
+    Counter heads = new Counter("heads");
+    Counter tails = new Counter("tails");
+
+    for (int t = 0; t < T; t++) {
+      if (StdRandom.bernoulli(.5))
+        heads.increment();
+      else
+        tails.increment();
+    }
+
+    if (heads.tally() == tails.tally())
+      System.out.println("tie!");
+    else 
+      System.out.println(max(heads, tails) + " wins!");
+  }
+}
---
+++ src/test/java/AlgorithmsPrincetonCourse/AbstractData/Exercises/InstrumentBinarySearch.java
@@ -0,0 +1,45 @@
+package AlgorithmsPrincetonCourse.AbstractData.Exercises;
+
+import java.util.Arrays;
+import AlgorithmsPrincetonCourse.AbstractData.Counter;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+
+/**
+ * <code>exercise-1.2.9</code>
+ */
+public class InstrumentBinarySearch {
+  public static int rank(int key, int[] a, Counter counter) { // Array must be sorted.
+    int lo = 0;
+    int hi = a.length - 1;
+
+    while (lo <= hi) { 
+      counter.increment();
+      // Key is in a[lo..hi] or not present.
+      int mid = lo + (hi - lo) / 2;
+      if (key < a[mid]) 
+        hi = mid - 1;
+      else if (key > a[mid]) 
+        lo = mid + 1;
+      else return mid;
+    }
+    return -1;
+  }
+
+  public static void main(String[] args){
+    Counter counter = new Counter("key examinations");
+    int[] whitelist = In.readInts("algs4-data/tinyW.txt");
+    In input = new In("algs4-data/tinyT.txt");
+    Arrays.sort(whitelist);
+
+    while (!input.isEmpty()) {
+      // Read key, print if not in whitelist.
+      int key = input.readInt();
+      if (rank(key, whitelist, counter) < 0)
+      StdOut.println(key);
+    }
+
+    System.out.println(counter);
+  }
+}
